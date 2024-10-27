478c333daf972066257e1c9c053438ea204ac237
Lemon <38927629+Lemonsity@users.noreply.github.com>
2024-10-27 01:37:19 -0400

    Working website

--- build-site.el
+++ build-site.el
@@ -10,10 +10,15 @@
 	     :publishing-directory "./public_html"
 	     :publish-function 'org-html-publish-to-html
 	     :with-author nil		; No author name
-	     :with-creator t		; Include Emacs and Org verison
+	     ;; :with-creator t		; Include Emacs and Org verison
 	     :with-toc nil		; Table of Content
 	     :section-numbers nil
-	     :time-stamp-file nil)
+	     :time-stamp-file nil
+	     
+	     ;; :auto-sitemap t                ; Generate sitemap.org automagically...
+	     ;; :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
+	     ;; :sitemap-title "Sitemap"         ; ... with title 'Sitemap'
+	     )
        (list "static"
 	     :base-directory "./content/"
 	     :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
@@ -26,7 +31,9 @@
 (setq org-html-validation-link nil	      ;; No "Validate" link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")	
+      org-html-head "<link rel=\"stylesheet\" href=\"/style.css\" />" 
+      ;; org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />"
+      )	
 
 (org-publish-all t)
 
---
+++ content/Blogs/auto/Ref.el
@@ -0,0 +1,10 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "Ref"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "DBLP:journals/corr/abs-1807-05923"
+    "pretnar_introduction_2015"))
+ '(or :bibtex :latex))
+
